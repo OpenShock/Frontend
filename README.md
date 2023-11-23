f4cbff13da52a11d7554eeb80a42ee0d88fcb838
hhvrc <github@heavenvr.tech>
2023-11-23 08:45:40 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# OpenShock Frontend
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import '../app.postcss';
+	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
+	import {
+		AppShell,
+		Modal,
+		Toast,
+		getToastStore,
+		initializeStores,
+		storePopup
+	} from '@skeletonlabs/skeleton';
+	import Footer from '$lib/components/layout/Footer.svelte';
+	import Header from '$lib/components/layout/Header.svelte';
+
+	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+	initializeStores();
+
+	const toastStore = getToastStore();
+</script>
+
+<Modal />
+<Toast position="bl" max={5} />
+
+<AppShell>
+	<Header slot="header" />
+	<slot />
+	<Footer slot="pageFooter" />
+</AppShell>
