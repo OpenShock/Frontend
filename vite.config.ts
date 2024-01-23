9d5e0ae80861af5dc0939c8f3a8eb4c4485baedd
hhvrc <github@heavenvr.tech>
2024-01-23 02:04:51 +0100

    Fix building

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,11 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit(), purgeCss()]
+	plugins: [sveltekit(), purgeCss({
+			safelist: {
+				// any selectors that begin with "hljs-" will not be purged
+				greedy: [/^hljs-/],
+			},
+		}),
+	],
 });
