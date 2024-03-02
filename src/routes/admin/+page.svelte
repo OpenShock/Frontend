6be5ae6bc2094b2ae12f3fbb9fc37331daf736b5
hhvrc <github@heavenvr.tech>
2024-03-02 01:01:41 +0100

    Improve AdminOnlineDevices table

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -1,49 +1,28 @@
 <script lang="ts">
   import { adminApi } from '$lib/api';
-  import { RankType, type AdminOnlineDeviceResponse as Device } from '$lib/api/internal/v1';
+  import { RankType, type AdminOnlineDeviceResponse } from '$lib/api/internal/v1';
   import { UserSelfStore } from '$lib/stores/UserStore';
+  import AdminDeviceList from './AdminDeviceList.svelte';
 
   $: isAdmin = $UserSelfStore?.rank === RankType.admin;
 
-  let devices: Device[] = [];
+  type FlatDevice = AdminOnlineDeviceResponse & { ownerName: string | null | undefined };
+
+  let devices: FlatDevice[] | null = null;
 
   $: if (isAdmin) {
     adminApi
       .adminGetOnlineDevices()
       .then((res) => {
-        devices = res.data ?? [];
+        devices =
+          res.data?.map((device) => ({
+            ...device,
+            ownerName: device.owner?.name,
+          })) ?? [];
       })
       .catch((err) => {
         console.error(err);
       });
-  }
-
-  let sortColumn: keyof Device = 'id';
-  let sortDirection: 'asc' | 'desc' = 'asc';
-
-  function rowClicked(key: keyof Device) {
-    if (sortColumn === key) {
-      sortDirection = sortDirection === 'asc' ? 'desc' : 'asc';
-    } else {
-      sortColumn = key;
-      sortDirection = 'asc';
-    }
-
-    let accessor: (d: Device) => string;
-    if (sortColumn === 'owner') {
-      accessor = (d) => d?.owner?.name?.toLowerCase() ?? '';
-    } else {
-      accessor = (d) => (d[sortColumn] as string | undefined | null)?.toLowerCase() ?? '';
-    }
-
-    let sorter: (a: Device, b: Device) => number;
-    if (sortDirection === 'asc') {
-      sorter = (a, b) => (accessor(a) > accessor(b) ? 1 : -1);
-    } else {
-      sorter = (a, b) => (accessor(a) < accessor(b) ? 1 : -1);
-    }
-
-    devices = devices.sort(sorter);
   }
 </script>
 
@@ -58,52 +37,11 @@
     <h2 class="h2">Admin Panel</h2>
 
     <!-- Online Users List -->
-    <div class="flex flex-col space-y-2 w-full">
-      <div class="flex items-center justify-between px-4">
-        <h3 class="h3">Online Users</h3>
-        <h4 class="h4">Total: {devices.length}</h4>
+    {#if devices}
+      <div class="flex flex-col space-y-2 w-full">
+        <h2 class="h2">Online Users</h2>
+        <AdminDeviceList {devices} />
       </div>
-      <div class="table-container">
-        <table class="table table-hover">
-          <thead>
-            <tr>
-              <th class="cursor-pointer select-none" on:click={() => rowClicked('id')}>
-                Device ID
-              </th>
-              <th class="cursor-pointer select-none" on:click={() => rowClicked('name')}>
-                Device Name
-              </th>
-              <th class="cursor-pointer select-none" on:click={() => rowClicked('firmwareVersion')}>
-                Firmware Version
-              </th>
-              <th class="cursor-pointer select-none" on:click={() => rowClicked('gateway')}>
-                Gateway
-              </th>
-              <th class="cursor-pointer select-none" on:click={() => rowClicked('owner')}>
-                Owner
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {#each devices as device}
-              <tr>
-                <td>{device.id}</td>
-                <td>{device.name}</td>
-                <td class={device.firmwareVersion ? '' : 'text-red-500'}>
-                  {device.firmwareVersion}
-                </td>
-                <td class={device.gateway ? '' : 'text-red-500'}>
-                  {device.gateway}
-                </td>
-                <td class="flex items-center space-x-2">
-                  <img class="h-8 rounded-full" src={device.owner?.image} alt="User Avatar" />
-                  <p>{device.owner?.name}</p>
-                </td>
-              </tr>
-            {/each}
-          </tbody>
-        </table>
-      </div>
-    </div>
+    {/if}
   {/if}
 </div>
---
+++ src/routes/admin/AdminDeviceList.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+  import { DataHandler } from '@vincjo/datatables';
+  import Pagination from '$lib/components/table/Pagination.svelte';
+  import RowCount from '$lib/components/table/RowCount.svelte';
+  import RowsPerPage from '$lib/components/table/RowsPerPage.svelte';
+  import Search from '$lib/components/table/Search.svelte';
+  import ThFilter from '$lib/components/table/ThFilter.svelte';
+  import ThSort from '$lib/components/table/ThSort.svelte';
+  import type { AdminOnlineDeviceResponse } from '$lib/api/internal/v1';
+
+  type FlatDevice = AdminOnlineDeviceResponse & { ownerName: string | null | undefined };
+
+  export let devices: FlatDevice[];
+
+  const handler = new DataHandler(devices, { rowsPerPage: 10 });
+  const rows = handler.getRows();
+</script>
+
+<div class="overflow-x-auto space-y-2">
+  <header class="flex justify-between gap-4">
+    <Search {handler} />
+    <RowsPerPage {handler} />
+  </header>
+  <table class="table table-hover table-compact table-auto w-full">
+    <thead>
+      <tr>
+        <ThSort {handler} orderBy="id">Device ID</ThSort>
+        <ThSort {handler} orderBy="name">Device Name</ThSort>
+        <ThSort {handler} orderBy="firmwareVersion">Firmware Version</ThSort>
+        <ThSort {handler} orderBy="gateway">Gateway</ThSort>
+        <ThSort {handler} orderBy="ownerName">Owner</ThSort>
+      </tr>
+      <tr>
+        <ThFilter {handler} filterBy="id" />
+        <ThFilter {handler} filterBy="name" />
+        <ThFilter {handler} filterBy="firmwareVersion" />
+        <ThFilter {handler} filterBy="gateway" />
+        <ThFilter {handler} filterBy="ownerName" />
+      </tr>
+    </thead>
+    <tbody>
+      {#each $rows as row}
+        <tr>
+          <td>{row.id}</td>
+          <td>{row.name}</td>
+          <td class={row.firmwareVersion ? '' : 'text-red-500'}>{row.firmwareVersion}</td>
+          <td class={row.gateway ? '' : 'text-red-500'}>{row.gateway}</td>
+          <td class="flex items-center space-x-2">
+            <img class="h-8 rounded-full" src={row.owner?.image} alt="User Avatar" />
+            <p>{row.ownerName}</p>
+          </td>
+        </tr>
+      {/each}
+    </tbody>
+  </table>
+  <footer class="flex justify-between">
+    <RowCount {handler} />
+    <Pagination {handler} />
+  </footer>
+</div>
