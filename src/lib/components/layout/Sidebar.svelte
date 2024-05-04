60f23de075448b281c2184c68e47708a2bc03fe6
hhvrc <github@heavenvr.tech>
2024-05-04 23:06:49 +0200

    Add titles to Sidebar items

--- src/lib/components/layout/Sidebar.svelte
+++ src/lib/components/layout/Sidebar.svelte
@@ -7,28 +7,43 @@
 {#if $UserSelfStore}
   <AppRail>
     <svelte:fragment slot="lead">
-      <AppRailAnchor href="/home" selected={$page.url.pathname === '/home'}>
-        <i class="fa fa-house fa-2xl" />
+      <AppRailAnchor href="/home" selected={$page.url.pathname === '/home'} title="Home">
+        <i slot="lead" class="fa fa-house fa-2xl py-4" />
+        <span>Home</span>
       </AppRailAnchor>
-      <AppRailAnchor href="/shockers" selected={$page.url.pathname === '/shockers'}>
-        <i class="fa fa-bolt fa-2xl" />
+      <AppRailAnchor
+        href="/shockers"
+        selected={$page.url.pathname === '/shockers'}
+        title="Shockers"
+      >
+        <i slot="lead" class="fa fa-bolt fa-2xl py-4" />
+        <span>Shockers</span>
       </AppRailAnchor>
-      <AppRailAnchor href="/devices" selected={$page.url.pathname === '/devices'}>
-        <i class="fa fa-microchip fa-2xl" />
+      <AppRailAnchor href="/devices" selected={$page.url.pathname === '/devices'} title="Devices">
+        <i slot="lead" class="fa fa-microchip fa-2xl py-4" />
+        <span>Devices</span>
       </AppRailAnchor>
-      <AppRailAnchor href="/sharelinks" selected={$page.url.pathname === '/sharelinks'}>
-        <i class="fa fa-link fa-2xl" />
+      <AppRailAnchor
+        href="/sharelinks"
+        selected={$page.url.pathname === '/sharelinks'}
+        title="Sharelinks"
+      >
+        <i slot="lead" class="fa fa-link fa-2xl py-4" />
+        <span>Sharelinks</span>
       </AppRailAnchor>
     </svelte:fragment>
     <svelte:fragment slot="trail">
-      <AppRailAnchor href="/tokens" selected={$page.url.pathname === '/tokens'}>
-        <i class="fa fa-key fa-2xl" />
+      <AppRailAnchor href="/tokens" selected={$page.url.pathname === '/tokens'} title="Tokens">
+        <i slot="lead" class="fa fa-key fa-2xl py-4" />
+        <span>Tokens</span>
       </AppRailAnchor>
-      <AppRailAnchor href="/account" selected={$page.url.pathname === '/account'}>
-        <i class="fa fa-user fa-2xl" />
+      <AppRailAnchor href="/account" selected={$page.url.pathname === '/account'} title="Account">
+        <i slot="lead" class="fa fa-user fa-2xl py-4" />
+        <span>Account</span>
       </AppRailAnchor>
-      <AppRailAnchor href="/admin" selected={$page.url.pathname === '/admin'}>
-        <i class="fa fa-user-shield fa-2xl" />
+      <AppRailAnchor href="/admin" selected={$page.url.pathname === '/admin'} title="Admin">
+        <i slot="lead" class="fa fa-user-shield fa-2xl py-4" />
+        <span>Admin</span>
       </AppRailAnchor>
     </svelte:fragment>
   </AppRail>
