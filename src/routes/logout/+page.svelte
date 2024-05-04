391bd9318fbd6473c49b6072a5171fe33fec79a7
hhvrc <github@heavenvr.tech>
2024-05-04 22:57:51 +0200

    Improve logout page for unauthenticated user

--- src/routes/logout/+page.svelte
+++ src/routes/logout/+page.svelte
@@ -1,22 +1,28 @@
 <script lang="ts">
   import { UserSelfStore } from '$lib/stores/UserStore';
   import { goto } from '$app/navigation';
-  import { onMount } from 'svelte';
 
   function logout() {
     // TODO: Make a API call to invalidate the cookie
 
     UserSelfStore.set(null);
+
     goto('/');
+  }
+
+  $: if (!$UserSelfStore) {
+    logout();
   }
 </script>
 
-<div class="container h-full mx-auto flex justify-center items-center">
-  <div class="flex flex-col space-y-4">
-    <h2 class="h2">Logout</h2>
+{#if $UserSelfStore}
+  <div class="container h-full mx-auto flex justify-center items-center">
+    <div class="flex flex-col space-y-4">
+      <h2 class="h2">Logout</h2>
 
-    <p>Are you sure you want to logout?</p>
+      <p>Are you sure you want to logout?</p>
 
-    <button class="btn variant-filled-primary" on:click={logout}> Logout </button>
+      <button class="btn variant-filled-primary" on:click={logout}> Logout </button>
+    </div>
   </div>
-</div>
+{/if}
