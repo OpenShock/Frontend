8f40869ca123f8932001305be0fb0010a51f710a
hhvrc <github@heavenvr.tech>
2024-05-05 00:09:52 +0200

    Fix text clipping issue in sidebar

--- src/lib/components/layout/Sidebar.svelte
+++ src/lib/components/layout/Sidebar.svelte
@@ -9,7 +9,7 @@
   <AppRail>
     <svelte:fragment slot="lead">
       <AppRailAnchor href="/home" selected={$page.url.pathname === '/home'} title="Home">
-        <i slot="lead" class="fa fa-house fa-2xl py-4" />
+        <i slot="lead" class="fa fa-house text-2xl" />
         <span>Home</span>
       </AppRailAnchor>
       <AppRailAnchor
@@ -17,11 +17,11 @@
         selected={$page.url.pathname === '/shockers'}
         title="Shockers"
       >
-        <i slot="lead" class="fa fa-bolt fa-2xl py-4" />
+        <i slot="lead" class="fa fa-bolt text-2xl" />
         <span>Shockers</span>
       </AppRailAnchor>
       <AppRailAnchor href="/devices" selected={$page.url.pathname === '/devices'} title="Devices">
-        <i slot="lead" class="fa fa-microchip fa-2xl py-4" />
+        <i slot="lead" class="fa fa-microchip text-2xl" />
         <span>Devices</span>
       </AppRailAnchor>
       <AppRailAnchor
@@ -29,22 +29,22 @@
         selected={$page.url.pathname === '/sharelinks'}
         title="Sharelinks"
       >
-        <i slot="lead" class="fa fa-link fa-2xl py-4" />
+        <i slot="lead" class="fa fa-link text-2xl" />
         <span>Sharelinks</span>
       </AppRailAnchor>
     </svelte:fragment>
     <svelte:fragment slot="trail">
       <AppRailAnchor href="/tokens" selected={$page.url.pathname === '/tokens'} title="Tokens">
-        <i slot="lead" class="fa fa-key fa-2xl py-4" />
+        <i slot="lead" class="fa fa-key text-2xl" />
         <span>Tokens</span>
       </AppRailAnchor>
       <AppRailAnchor href="/account" selected={$page.url.pathname === '/account'} title="Account">
-        <i slot="lead" class="fa fa-user fa-2xl py-4" />
+        <i slot="lead" class="fa fa-user text-2xl" />
         <span>Account</span>
       </AppRailAnchor>
       {#if $UserSelfStore.rank === RankType.admin}
         <AppRailAnchor href="/admin" selected={$page.url.pathname === '/admin'} title="Admin">
-          <i slot="lead" class="fa fa-user-shield fa-2xl py-4" />
+          <i slot="lead" class="fa fa-user-shield text-2xl" />
           <span>Admin</span>
         </AppRailAnchor>
       {/if}
