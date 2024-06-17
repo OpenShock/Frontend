d2ec991be81b288c0c9a4e4cd2d5c2d8318ae366
hhvrc <contact@heavenvr.tech>
2024-06-17 12:53:07 +0200

    Yeet ShockLink references

--- src/lib/api/internal/v2/runtime.ts
+++ src/lib/api/internal/v2/runtime.ts
@@ -13,7 +13,7 @@
  */
 
 
-export const BASE_PATH = "https://api.shocklink.net".replace(/\/+$/, "");
+export const BASE_PATH = "https://api.openshock.app".replace(/\/+$/, "");
 
 export interface ConfigurationParameters {
     basePath?: string; // override base path
