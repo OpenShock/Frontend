6b32cd9045695eeef1c975ba1f6e5d35abfe0c4f
hhvrc <github@heavenvr.tech>
2024-10-14 13:31:57 +0200

    Update OpenAPI schemas

--- src/lib/api/internal/v2/models/index.ts
+++ src/lib/api/internal/v2/models/index.ts
@@ -1,5 +1,6 @@
 /* tslint:disable */
 /* eslint-disable */
+export * from './ChangeUsernameRequest';
 export * from './Control';
 export * from './ControlRequest';
 export * from './ControlType';
@@ -7,3 +8,8 @@ export * from './LoginV2';
 export * from './ObjectBaseResponse';
 export * from './OpenShockProblem';
 export * from './SignUpV2';
+export * from './UsernameAvailability';
+export * from './UsernameCheckResponse';
+export * from './UsernameCheckResponseBaseResponse';
+export * from './UsernameError';
+export * from './UsernameErrorType';
