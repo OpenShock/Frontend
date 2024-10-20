01f9b6e5a50adfa39165ffb52d5ad4c404d85256
hhvrc <github@heavenvr.tech>
2024-10-20 05:41:16 +0200

    Use snippets in Header

--- src/lib/components/layout/Header.svelte
+++ src/lib/components/layout/Header.svelte
@@ -10,6 +10,18 @@
   };
 </script>
 
+{#snippet item(text: string, href: string)}
+  <a {href} class="block text-gray-600 hover:text-gray-800 dark:text-gray-400 dark:hover:text-gray-200">
+    {text}
+  </a>
+{/snippet}
+
+{#snippet brand(name: string, icon: `fa-${string}`, href: string)}
+  <a href={href} class="btn-icon" aria-label={name}>
+    <i class="fa-brands {icon} text-lg"></i>
+  </a>
+{/snippet}
+
 <AppBar>
   {#snippet lead()}
     <div  class="flex items-center space-x-4">
@@ -25,59 +37,29 @@
     </div>
   {/snippet}
   {#snippet trail()}
-  
-      <LightSwitch />
-      {#if $UserStore.self !== null}
-        <div
-          class="text-gray-600 hover:text-gray-800 dark:text-gray-400 dark:hover:text-gray-200 cursor-pointer"
-          use:popup={accountPopup}
-        >
-          <img class="inline-block h-8 rounded-full" src={$UserStore.self.avatar} alt="User Avatar" />
-          <p class="hidden lg:inline-block">{$UserStore.self.name}</p>
-        </div>
-        <div class="card p-4 w-48 bg-surface-200-700-token shadow-xl" data-popup="accountPopup">
-          <a
-            href="/profile"
-            class="block text-gray-600 hover:text-gray-800 dark:text-gray-400 dark:hover:text-gray-200"
-          >
-            Profile
-          </a>
-          <a
-            href="/settings"
-            class="block text-gray-600 hover:text-gray-800 dark:text-gray-400 dark:hover:text-gray-200"
-          >
-            Settings
-          </a>
-          <a
-            href="/logout"
-            class="block text-gray-600 hover:text-gray-800 dark:text-gray-400 dark:hover:text-gray-200"
-          >
-            Logout
-          </a>
-          <div class="arrow bg-surface-200-700-token" data-arrow="accountPopup"></div>
-        </div>
-      {:else}
-        <a
-          href="/login"
-          class="text-gray-600 hover:text-gray-800 dark:text-gray-400 dark:hover:text-gray-200"
-        >
-          Login
-        </a>
-        <a
-          href="/signup"
-          class="text-gray-600 hover:text-gray-800 dark:text-gray-400 dark:hover:text-gray-200"
-        >
-          Sign Up
-        </a>
-        <div>
-          <a href={PUBLIC_GITHUB_PROJECT_URL} class="btn-icon" aria-label="GitHub">
-            <i class="fa-brands fa-github text-lg"></i>
-          </a>
-          <a href={PUBLIC_DISCORD_INVITE_URL} class="btn-icon" aria-label="Discord">
-            <i class="fa-brands fa-discord text-lg"></i>
-          </a>
-        </div>
-      {/if}
+    <LightSwitch />
     
+    {#if $UserStore.self !== null}
+      <div
+        class="text-gray-600 hover:text-gray-800 dark:text-gray-400 dark:hover:text-gray-200 cursor-pointer"
+        use:popup={accountPopup}
+      >
+        <img class="inline-block h-8 rounded-full" src={$UserStore.self.avatar} alt="User Avatar" />
+        <p class="hidden lg:inline-block">{$UserStore.self.name}</p>
+      </div>
+      <div class="card p-4 w-48 bg-surface-200-700-token shadow-xl" data-popup="accountPopup">
+        {@render item('Profile', '/profile')}
+        {@render item('Settings', '/settings')}
+        {@render item('Logout', '/logout')}
+        <div class="arrow bg-surface-200-700-token" data-arrow="accountPopup"></div>
+      </div>
+    {:else}
+      {@render item('Login', '/login')}
+      {@render item('Sign Up', '/signup')}
+      <div>
+        {@render brand('GitHub', 'fa-github', PUBLIC_GITHUB_PROJECT_URL)}
+        {@render brand('Discord', 'fa-discord', PUBLIC_DISCORD_INVITE_URL)}
+      </div>
+    {/if}
   {/snippet}
 </AppBar>
