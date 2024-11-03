6dd34184db2f5d7ae68f2f2ce4ad7f1bf092ff2b
hhvrc <github@heavenvr.tech>
2024-11-03 22:49:25 +0100

    Improve password strength meter

--- src/lib/components/input/impl/PasswordStrengthMeter.svelte
+++ src/lib/components/input/impl/PasswordStrengthMeter.svelte
@@ -1,28 +1,18 @@
 <script lang="ts">
   import { getPasswordStrength } from '$lib/inputvalidation/passwordValidator';
 
-  interface Props {
-    popupTarget: string;
-    password: string;
-  }
+  let { password }: { password: string; } = $props();
 
-  let { popupTarget, password }: Props = $props();
-
-  let arrow: HTMLDivElement | undefined = $state();
-
-  let strength = $derived(getPasswordStrength(password));
-  $effect(() => {
-    if (arrow) arrow.style.left = `${strength.percent}%`;
-  });
+  let { percent, text, color } = $derived(getPasswordStrength(password));
 </script>
 
-<div class="card p-4 w-72 shadow-xl" data-popup={popupTarget}>
+<div class="card p-4 w-72 shadow-xl">
   <div class="flex flex-row items-center space-x-1">
     <p class="text-sm text-gray-500">Password strength:</p>
-    <p class={`text-sm text-${strength.color}`}>{strength.text}</p>
+    <p class={`text-sm text-${color}`}>{text}</p>
   </div>
   <div class="arrow bg-surface-100-800-token"></div>
-  <div class="pt-2 h-1 w-full flex flex-row items-center space-x-1">
+  <div id="line">
     <div class="h-1 bg-red-500" style="flex: 3 3 0%"></div>
     <div class="h-1 bg-orange-500" style="flex: 2 2 0%"></div>
     <div class="h-1 bg-yellow-500" style="flex: 1 1 0%"></div>
@@ -31,8 +21,16 @@
   <!-- Arrow -->
   <div class="relative w-full h-0">
     <div
-      bind:this={arrow}
       class="absolute w-2 h-2 bg-black dark:bg-white transform rotate-45 -translate-x-1 -translate-y-1"
+      style={`left: ${percent}%`}
     ></div>
   </div>
 </div>
+
+<style lang="postcss">
+  #line {
+    @apply pt-2 h-1 w-full flex flex-row items-center space-x-1;
+  }
+
+  #line div
+</style>
