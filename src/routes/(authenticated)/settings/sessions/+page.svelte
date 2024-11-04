ddb51b0188430c8879ac1ecd07e6d977f307f834
hhvrc <github@heavenvr.tech>
2024-11-04 12:51:18 +0100

    Sessions page stuffz

--- src/routes/(authenticated)/settings/sessions/+page.svelte
+++ src/routes/(authenticated)/settings/sessions/+page.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
   import { sessionApi } from '$lib/api';
-  import type { LoginSessionResponse} from '$lib/api/internal/v1';
+  import type { LoginSessionResponse } from '$lib/api/internal/v1';
   import * as Dialog from '$lib/components/ui/dialog';
   import { handleApiError } from '$lib/errorhandling/apiErrorHandling';
-  import {onDestroy, onMount} from 'svelte';
-  import {columns, type Session} from "./columns";
-  import DataTable from "./data-table.svelte";
+  import { onDestroy, onMount } from 'svelte';
+  import { columns, type Session } from './columns';
+  import DataTable from './data-table.svelte';
+
+  import RotateCcw from 'lucide-svelte/icons/rotate-ccw';
+  import Button from '$lib/components/ui/button/button.svelte';
 
   function apiSessionToTableDevice(session: LoginSessionResponse): Session {
     return {
@@ -13,7 +16,7 @@
       ip: session.ip,
       user_agent: session.userAgent,
       created_at: session.created,
-      expires_at: session.expires
+      expires_at: session.expires,
     };
   }
 
@@ -42,11 +45,15 @@
   onMount(() => {
     fetchSessions();
     // Trigger refresh every 5 seconds
-    interval = setInterval(() => { if (sessions) { sessions = Object.assign([], sessions); } }, 5000);
+    interval = setInterval(() => {
+      if (sessions) {
+        sessions = Object.assign([], sessions);
+      }
+    }, 5000);
   });
   onDestroy(() => {
     clearInterval(interval);
-  })
+  });
 </script>
 
 {#if sessionToDelete != null}
@@ -54,11 +61,12 @@
     <Dialog.Trigger>Open</Dialog.Trigger>
     <Dialog.Content>
       <Dialog.Header>
-        <Dialog.Title>Are you sure you want to log out from <b>{sessionToDelete.userAgent}</b>?</Dialog.Title>
+        <Dialog.Title
+          >Are you sure you want to log out from <b>{sessionToDelete.userAgent}</b>?</Dialog.Title
+        >
         <Dialog.Description>
-          !!! Testing text !!!
-          This action cannot be undone. This will permanently delete your account
-          and remove your data from our servers.
+          !!! Testing text !!! This action cannot be undone. This will permanently delete your
+          account and remove your data from our servers.
         </Dialog.Description>
       </Dialog.Header>
     </Dialog.Content>
@@ -67,14 +75,13 @@
 
 <div class="flex justify-between w-full mb-2">
   <h2 class="text-3xl">Sessions</h2>
-  <button class="btn variant-filled-primary text-xl" onclick={fetchSessions}>
-    <i class="fa fa-sync"></i>
-    Refresh
-  </button>
+  <Button class="btn variant-filled-primary text-xl" onclick={fetchSessions}>
+    <RotateCcw />
+    <span> Refresh </span>
+  </Button>
 </div>
 <p>Currently valid sessions</p>
 
 {#if sessions}
   <DataTable {sessions} {columns} />
 {/if}
-
