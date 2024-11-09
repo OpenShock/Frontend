41591637421b4dc6e79b4b4152e9d0dcd714999d
hhvrc <github@heavenvr.tech>
2024-11-09 22:49:11 +0100

    Improve api error handling

--- src/lib/errorhandling/apiErrorHandling.ts
+++ src/lib/errorhandling/apiErrorHandling.ts
@@ -1,28 +1,51 @@
-import { ResponseError } from "$lib/api/internal/v1";
+import { FetchError, RequiredError, ResponseError } from "$lib/api/internal/v1";
+import { toast } from "svelte-sonner";
 import type { ProblemDetails } from "./ProblemDetails";
-import { toast } from 'svelte-sonner';
 
 
 export type HandleProblemCallback = (problem: ProblemDetails) => boolean;
 
 export async function handleApiError(error: any, handleProblemCallback: HandleProblemCallback | null = null): Promise<void> {
-  if (!(error instanceof ResponseError)) {
+  if (error instanceof ResponseError) {
+    const problem = await getProblemDetails(error.response);
+  
+    if (problem === null) {
+      console.error('Got error with problem of null', error); // TODO: Display toast
+      return;
+    }
+  
+    console.error("API error: " + problem.title);
+  
+    if (handleProblemCallback && handleProblemCallback(problem)) {
+      // TODO: Display toast
+      return;
+    }
+  
+    toast.error(problem.title);
     return;
   }
 
-  const problem = await getProblemDetails(error.response);
-
-  if (problem === null) {
+  if (error instanceof FetchError) {
+    console.error('Got fetch error: ' + JSON.stringify(error)); // TODO: Display toast
     return;
   }
 
-  console.error("API error: " + problem.title);
-
-  if (handleProblemCallback && handleProblemCallback(problem)) {
+  if (error instanceof RequiredError) {
+    console.error('Got required error: ' + JSON.stringify(error)); // TODO: Display toast
     return;
   }
 
-  toast.error(problem.title);
+  if (error instanceof Error) {
+    console.error('Got generic error: ' + JSON.stringify(error)); // TODO: Display toast
+    return;
+  }
+  
+  if ('cause' in error) {
+    console.error('Got unknown error type: ' + JSON.stringify(error)); // TODO: Display toast
+    return;
+  }
+
+  console.error('Got error of unknown type', JSON.stringify(error)); // TODO: Display toast
 }
 
 async function getProblemDetails(response: Response): Promise<ProblemDetails | null> {
