29295443e7d4b88dbeb3637c1b62e49b0715c2bf
hhvrc <github@heavenvr.tech>
2025-01-08 15:31:34 +0100

    Update password validator to adhere to OWASP ASVS v4.0.3 recommendations

--- src/lib/inputvalidation/passwordValidator.ts
+++ src/lib/inputvalidation/passwordValidator.ts
@@ -2,33 +2,6 @@ import type { TwColor } from '$lib/types/Tailwind';
 import type { ValidationResult } from '$lib/types/ValidationResult';
 import { calculateStringEntropy } from '$lib/utils/entropy';
 
-function countCharacters(value: string): {
-  lower: number;
-  upper: number;
-  digit: number;
-  special: number;
-} {
-  const uniqueChars = new Set(value);
-  let lower = 0;
-  let upper = 0;
-  let digit = 0;
-  let special = 0;
-
-  for (const char of uniqueChars) {
-    if (char.match(/[a-z]/)) {
-      lower++;
-    } else if (char.match(/[A-Z]/)) {
-      upper++;
-    } else if (char.match(/[0-9]/)) {
-      digit++;
-    } else if ('!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'.includes(char)) {
-      special++;
-    }
-  }
-
-  return { lower, upper, digit, special };
-}
-
 export function validatePassword(value: string): ValidationResult | null {
   if (value.length == 0) {
     return null;
@@ -57,27 +30,6 @@ export function validatePassword(value: string): ValidationResult | null {
 
   if (trimmedLength != value.length) {
     result.message = 'Password cannot start or end with whitespace';
-    return result;
-  }
-
-  const { lower, upper, digit, special } = countCharacters(value);
-  if (lower < 1) {
-    result.message = 'Password must contain a lowercase character';
-    return result;
-  }
-
-  if (upper < 1) {
-    result.message = 'Password must contain a uppercase character';
-    return result;
-  }
-
-  if (digit < 1) {
-    result.message = 'Password must contain a digit';
-    return result;
-  }
-
-  if (special < 1) {
-    result.message = 'Password must contain a special character';
     return result;
   }
 
