b85aa855a339a12fdb6b648075c8c396332339d4
hhvrc <github@heavenvr.tech>
2025-02-05 19:54:31 +0100

    Sidebar ease in out

--- src/lib/components/ui/sidebar/sidebar.svelte
+++ src/lib/components/ui/sidebar/sidebar.svelte
@@ -63,7 +63,7 @@
 		<!-- This is what handles the sidebar gap on desktop -->
 		<div
 			class={cn(
-				"relative h-svh w-[--sidebar-width] bg-transparent transition-[width] duration-200 ease-linear",
+				"relative h-svh w-[--sidebar-width] bg-transparent transition-[width] duration-200 ease-in-out",
 				"group-data-[collapsible=offcanvas]:w-0",
 				"group-data-[side=right]:rotate-180",
 				variant === "floating" || variant === "inset"
@@ -73,7 +73,7 @@
 		></div>
 		<div
 			class={cn(
-				"fixed inset-y-0 z-10 hidden h-svh w-[--sidebar-width] transition-[left,right,width] duration-200 ease-linear md:flex",
+				"fixed inset-y-0 z-10 hidden h-svh w-[--sidebar-width] transition-[left,right,width] duration-200 ease-in-out md:flex",
 				side === "left"
 					? "left-0 group-data-[collapsible=offcanvas]:left-[calc(var(--sidebar-width)*-1)]"
 					: "right-0 group-data-[collapsible=offcanvas]:right-[calc(var(--sidebar-width)*-1)]",
