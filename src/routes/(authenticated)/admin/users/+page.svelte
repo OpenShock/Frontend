cfc9225fcbc8cc8013a83a969df2dd10dd8424b5
hhvrc <github@heavenvr.tech>
2025-03-24 08:30:09 +0100

    Start making searchable feature

--- src/routes/(authenticated)/admin/users/+page.svelte
+++ src/routes/(authenticated)/admin/users/+page.svelte
@@ -1,12 +1,35 @@
 <script lang="ts">
+  import {
+    getCoreRowModel,
+    getFilteredRowModel,
+    getPaginationRowModel,
+    getSortedRowModel,
+    type ColumnFiltersState,
+    type PaginationState,
+    type SortingState,
+  } from '@tanstack/table-core';
   import { adminApi } from '$lib/api';
   import type { AdminUsersView } from '$lib/api/internal/v1/models/AdminUsersView';
-  import { handleApiError } from '$lib/errorhandling/apiErrorHandling';
   import { Button } from '$lib/components/ui/button';
-  import * as Card from '$lib/components/ui/card';
-  import { onDestroy, onMount } from 'svelte';
+  import {
+    CardHeader,
+    CardTitle,
+    CardContent
+  } from '$lib/components/ui/card';
+  import {
+    Pagination,
+    PaginationContent,
+    PaginationEllipsis,
+    PaginationItem,
+    PaginationLink,
+    PaginationNextButton,
+    PaginationPrevButton,
+  } from '$lib/components/ui/pagination';
+  import { handleApiError } from '$lib/errorhandling/apiErrorHandling';
+  import Table from '$lib/components/Table/TableTemplate.svelte';
+  import { Input } from '$lib/components/ui/input';
+  import { createSvelteTable } from '$lib/components/ui/data-table';
   import { columns, type User } from './columns';
-  import DataTable from './data-table.svelte';
 
   import RotateCcw from '@lucide/svelte/icons/rotate-ccw';
 
@@ -40,43 +63,127 @@
   type FilterMap<TEntity> = { [K in keyof TEntity]: string };
   type OrderbyQuery<TEntity> = `${Extract<keyof TEntity, string>} ${'asc' | 'desc'}`;
 
+  const PerPage = 200;
+
+  let total = $state(0);
   let data = $state<User[]>([]);
 
-  function fetchUsers() {
+  let page = $state(1);
+
+  let nameFilterValue = $state('');
+  let emailFilterValue = $state('');
+
+  function fetchUsers($filter: string, $orderby: string, $page: number) {
     adminApi
-      .adminGetUsers() /* filter, orderby, offset, limit */
+      .adminGetUsers($filter, $orderby, ($page - 1) * PerPage, PerPage) /* filter, orderby, offset, limit */
       .then((res) => {
         if (res.data) {
+          total = res.total;
           data = res.data.map(apiUserToTableDevice);
         }
       })
       .catch(handleApiError);
   }
 
-  let interval: ReturnType<typeof setInterval>;
-  onMount(() => {
-    fetchUsers();
-    // Update timestamps every minute
-    interval = setInterval(() => {
-      data = Object.assign([], data);
-    }, 60000);
-  });
-  onDestroy(() => {
-    clearInterval(interval);
+  $effect(() => fetchUsers('', 'name asc', page));
+
+  let pagination = $state<PaginationState>({ pageIndex: 0, pageSize: PerPage });
+  let sorting = $state<SortingState>([]);
+  let columnFilters = $state<ColumnFiltersState>([]);
+
+  const table = createSvelteTable({
+    get data() {
+      return data;
+    },
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    onPaginationChange: (updater) => {
+      if (typeof updater === 'function') {
+        pagination = updater(pagination);
+      } else {
+        pagination = updater;
+      }
+    },
+    onSortingChange: (updater) => {
+      if (typeof updater === 'function') {
+        sorting = updater(sorting);
+      } else {
+        sorting = updater;
+      }
+    },
+    onColumnFiltersChange: (updater) => {
+      if (typeof updater === 'function') {
+        columnFilters = updater(columnFilters);
+      } else {
+        columnFilters = updater;
+      }
+    },
+    state: {
+      get pagination() {
+        return pagination;
+      },
+      get sorting() {
+        return sorting;
+      },
+      get columnFilters() {
+        return columnFilters;
+      },
+    },
   });
 </script>
 
 <div class="container my-8">
-  <Card.Header>
-    <Card.Title class="flex items-center justify-between space-x-2 text-3xl">
+  <CardHeader>
+    <CardTitle class="flex items-center justify-between space-x-2 text-3xl">
       Users
+    </CardTitle>
+  </CardHeader>
+  <CardContent>
+    <div class="flex items-center space-x-4 py-4">
+      <Input
+        placeholder="Filter names..."
+        bind:value={nameFilterValue}
+        class="max-w-sm"
+      />
+      <Input
+        placeholder="Filter emails..."
+        bind:value={emailFilterValue}
+        class="max-w-sm"
+      />
+      <div class="flex-1"></div>
       <Button class="btn variant-filled-primary text-xl" onclick={fetchUsers}>
         <RotateCcw />
         <span> Refresh </span>
       </Button>
-    </Card.Title>
-  </Card.Header>
-  <Card.Content>
-    <DataTable {data} {columns} />
-  </Card.Content>
+    </div>
+    <Table {table} {columns} />
+  </CardContent>
+  <Pagination count={total} perPage={PerPage} bind:page>
+    {#snippet children({ pages, currentPage })}
+      <PaginationContent>
+        <PaginationItem>
+          <PaginationPrevButton />
+        </PaginationItem>
+        {#each pages as page (page.key)}
+          {#if page.type === 'ellipsis'}
+            <PaginationItem>
+              <PaginationEllipsis />
+            </PaginationItem>
+          {:else}
+            <PaginationItem>
+              <PaginationLink {page} isActive={currentPage === page.value}>
+                {page.value}
+              </PaginationLink>
+            </PaginationItem>
+          {/if}
+        {/each}
+        <PaginationItem>
+          <PaginationNextButton />
+        </PaginationItem>
+      </PaginationContent>
+    {/snippet}
+  </Pagination>
 </div>
