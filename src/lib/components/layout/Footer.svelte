5b381e80e4711fd0ddba8b2f39ea697f72aa00cc
hhvrc <github@heavenvr.tech>
2025-05-21 22:38:39 +0200

    Show SignalR state in lower-right corner

--- src/lib/components/layout/Footer.svelte
+++ src/lib/components/layout/Footer.svelte
@@ -1,11 +1,20 @@
 <script lang="ts">
   import { PUBLIC_GITHUB_PROJECT_URL } from '$env/static/public';
+  import { SignalR_State } from '$lib/signalr';
+  import { HubConnectionState } from '@microsoft/signalr';
 </script>
 
-<footer class="bottom-0 m-2 flex items-center justify-center sm:justify-between">
+<footer class="bottom-0 flex items-center justify-center px-2 text-sm sm:justify-between">
   <div>
     Made with
     <span style="color: #e25555;">&#9829;</span>
     by the <a target="_blank" href={PUBLIC_GITHUB_PROJECT_URL}> OpenShock Team</a>
+  </div>
+  <div>
+    <span
+      class={$SignalR_State === HubConnectionState.Connected ? 'text-green-500' : 'text-red-500'}
+    >
+      {$SignalR_State}
+    </span>
   </div>
 </footer>
