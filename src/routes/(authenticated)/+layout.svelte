d845bf4d0bf555a7d7b24d9b1277a9307a892ada
hhvrc <github@heavenvr.tech>
2025-05-21 22:37:03 +0200

    Improve loading screen

--- src/routes/(authenticated)/+layout.svelte
+++ src/routes/(authenticated)/+layout.svelte
@@ -1,8 +1,7 @@
 <script lang="ts">
+  import LoadingCircle from '$lib/components/svg/LoadingCircle.svelte';
   import { Button } from '$lib/components/ui/button';
-  import { SignalR_State } from '$lib/signalr';
   import { UserStore } from '$lib/stores/UserStore';
-  import { HubConnectionState } from '@microsoft/signalr';
   import type { Snippet } from 'svelte';
 
   interface Props {
@@ -12,14 +11,14 @@
   let { children }: Props = $props();
 </script>
 
-{#if $UserStore.self === null}
+{#if $UserStore.loading}
+  <div class="container mx-auto flex h-full flex-col items-center justify-center gap-4">
+    <LoadingCircle class="size-20" />
+  </div>
+{:else if $UserStore.self === null}
   <div class="container mx-auto flex h-full flex-col items-center justify-center gap-4">
     <h1 class="text-4xl">You need to be logged in to access this page</h1>
     <Button href="/login">Login</Button>
-  </div>
-{:else if $SignalR_State !== HubConnectionState.Connected}
-  <div class="container mx-auto flex h-full items-center justify-center">
-    <h1 class="text-4xl">Connecting to server...</h1>
   </div>
 {:else}
   {@render children?.()}
