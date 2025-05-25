d967b547f8199639bf57ffef609b44b377332758
hhvrc <github@heavenvr.tech>
2025-05-25 22:15:53 +0200

    :)

---
+++ src/routes/(authenticated)/+layout.server.ts
@@ -0,0 +1,7 @@
+import { redirect } from '@sveltejs/kit';
+
+export function load({ cookies, url }) {
+	if (!cookies.get('openShockSession')) {
+		redirect(303, `/login?redirectTo=${url.pathname}`);
+	}
+}
