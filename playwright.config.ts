442ae6a976e3485f966a458b33745a66f5d31032
hhvrc <github@heavenvr.tech>
2025-06-03 15:41:18 +0200

    Attempt to get playwright workig (its stubborn)

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
+  reporter: process.env.CI ? 'github' : 'html',
+  use: {
+    baseURL: 'https://local.openshock.app:4173',
+    trace: 'on-first-retry',
+  },
   webServer: {
-    command: 'npm run build && npm run preview',
+    command: 'pnpm run build && pnpm run preview',
     port: 4173,
   },
   testDir: 'e2e',
