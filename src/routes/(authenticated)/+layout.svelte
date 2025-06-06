d9ab73f8baca9dbef6ebe307a33da60cf6b2c278
hhvrc <github@heavenvr.tech>
2025-06-06 20:05:36 +0200

    Fix auth loading state a bit

--- src/routes/(authenticated)/+layout.svelte
+++ src/routes/(authenticated)/+layout.svelte
@@ -1,29 +1,20 @@
 <script lang="ts">
   import { goto } from '$app/navigation';
-  import { page } from '$app/state';
   import Container from '$lib/components/Container.svelte';
   import LoadingCircle from '$lib/components/svg/LoadingCircle.svelte';
-  import { Button } from '$lib/components/ui/button';
   import { UserStore } from '$lib/stores/UserStore';
   import type { Snippet } from 'svelte';
 
   let { children }: { children: Snippet } = $props();
 
   $effect(() => {
-    if (!$UserStore.loading && !$UserStore.self) {
-      goto(page.url.searchParams.get('redirect') ?? '/login');
-    }
+    if (!$UserStore.loading && !$UserStore.self) goto('/login');
   });
 </script>
 
-{#if $UserStore.loading}
+{#if !$UserStore.self}
   <Container>
     <LoadingCircle class="size-20" />
-  </Container>
-{:else if $UserStore.self === null}
-  <Container>
-    <h1 class="text-4xl">You need to be logged in to access this page</h1>
-    <Button href="/login">Login</Button>
   </Container>
 {:else}
   {@render children()}
