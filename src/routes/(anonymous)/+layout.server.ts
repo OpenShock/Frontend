8a9604d9c5acc50001ee72ecdaab6544aee3adfc
hhvrc <github@heavenvr.tech>
2025-06-06 19:43:27 +0200

    This should be fineeee

--- src/routes/(anonymous)/+layout.server.ts
+++ src/routes/(anonymous)/+layout.server.ts
@@ -3,6 +3,6 @@ import { redirect } from '@sveltejs/kit';
 export async function load({ cookies, url }) {
   const cookieValue = cookies.get('openShockSession');
   if (cookieValue !== undefined && cookieValue !== null) {
-    redirect(302, url.searchParams.get('redirect') ?? '/home');
+    redirect(303, url.searchParams.get('redirect') ?? '/home');
   }
 }
--- src/routes/(authenticated)/+layout.ts
+++ src/routes/(authenticated)/+layout.ts
@@ -1,25 +1,3 @@
-import { browser } from '$app/environment';
-import { goto } from '$app/navigation';
-import { destroyAuth } from '$lib/init';
-import { UserStore } from '$lib/stores/UserStore';
-import { get } from 'svelte/store';
-
 // The pages below this one will be different from user-to-user so cannot be prerendered and really shouldnt be server rendered
 export const ssr = false; // Only render authenticated pages in browser
 export const prerender = false;
-
-// Initialize stores and signalr on auth
-export async function load({ url }) {
-  if (!browser) return; // Yeah please dont run on server
-
-  // On loading in the anonymous section, check if cookie is set, if it is send us to the authenticated section
-  if (!get(UserStore).self) {
-    try {
-      await destroyAuth();
-    } catch (error) {
-      console.error(error);
-    } finally {
-      goto(`/login?redirect=${url.pathname}`);
-    }
-  }
-}
