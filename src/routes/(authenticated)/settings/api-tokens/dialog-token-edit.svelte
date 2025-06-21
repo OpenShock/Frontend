0980b009e74657f823006c76c868ce7144558d57
LucHeart <luc@luc.cat>
2025-06-22 00:40:06 +0200

    Fix api token edit dialog

--- src/routes/(authenticated)/settings/api-tokens/dialog-token-edit.svelte
+++ src/routes/(authenticated)/settings/api-tokens/dialog-token-edit.svelte
@@ -8,6 +8,7 @@
   import type { ValidationResult } from '$lib/types/ValidationResult';
   import { toast } from 'svelte-sonner';
   import type { ApiToken } from './columns';
+  import { onMount } from 'svelte';
 
   interface Props {
     open: boolean;
@@ -41,16 +42,19 @@
     perms: { name: string; key: string }[];
   };
 
-  const permissionTypes = Object.values(PermissionType).reduce((acc: PermissionCategory[], v) => {
-    const [category, perm] = v.split('.');
-    const cat = acc.find((c) => c.name === category);
-    if (cat) {
-      cat.perms.push({ name: capitalizeFirstLetter(perm), key: v });
-    } else {
-      acc.push({ name: capitalizeFirstLetter(category), perms: [{ name: perm, key: v }] });
-    }
-    return acc;
-  }, []);
+  const permissionCategories = Object.values(PermissionType).reduce(
+    (acc: PermissionCategory[], v) => {
+      const [category, perm] = v.split('.');
+      const cat = acc.find((c) => c.name === category);
+      if (cat) {
+        cat.perms.push({ name: perm, key: v });
+      } else {
+        acc.push({ name: category, perms: [{ name: perm, key: v }] });
+      }
+      return acc;
+    },
+    []
+  );
 
   function nameValidation(name: string): ValidationResult {
     if (name.length === 0) {
@@ -65,6 +69,11 @@
   }
 
   let nameValidationResult = $derived(nameValidation(name));
+
+  onMount(() => {
+    name = token.name;
+    permissions = token.permissions;
+  });
 </script>
 
 <Dialog.Root bind:open={() => open, (o) => (open = o)}>
@@ -73,28 +82,23 @@
       <Dialog.Title>Edit API Token</Dialog.Title>
       <Dialog.Description></Dialog.Description>
     </Dialog.Header>
-    <form class="space-y-8">
+    <form class="modal-form border-surface-500 rounded-container-token space-y-4">
       <TextInput
         label="Token Name"
-        placeholder="Enter a Token name... e.g. ShockOSC"
+        placeholder="Token name..."
         bind:value={name}
         validationResult={nameValidationResult}
       />
 
-      <div>
+      <div class="mt-4">
         <h2>Permissions</h2>
-        <div class="border-surface-500 space-y-4 rounded-md border p-4">
-          {#each permissionTypes as category}
-            <span>{category.name}</span>
-            {#each category.perms as permission}
+        <div class="border-surface-500 mt-3 flex flex-col space-y-4 rounded-md border p-4">
+          {#each permissionCategories as permission}
+            <span>{capitalizeFirstLetter(permission.name)}</span>
+            {#each permission.perms as perm}
               <label class="mt-0! ml-4">
-                <input
-                  type="checkbox"
-                  class="checkbox"
-                  value={permission.key}
-                  bind:group={permissions}
-                />
-                {permission.name}
+                <input type="checkbox" class="checkbox" value={perm.key} bind:group={permissions} />
+                {capitalizeFirstLetter(perm.name)}
               </label>
             {/each}
           {/each}
