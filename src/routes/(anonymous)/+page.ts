6cd3bf4038ac794309f8ab87e3e817489285def6
hhvrc <github@heavenvr.tech>
2025-07-17 16:11:27 +0200

    Oops

--- src/routes/(anonymous)/+page.ts
+++ src/routes/(anonymous)/+page.ts
@@ -1,27 +1,2 @@
-import { metaApi } from '$lib/api';
-
 export const ssr = true;
 export const prerender = false;
-
-export async function load({ setHeaders }): Promise<{ ok: false, error: string } | { ok: true, deviceCount: number }> {
-  setHeaders({
-    'cache-control': 'public, max-age=300'
-  })
-
-  let deviceCount: number;
-
-  try {
-    const { data } = await metaApi.publicGetOnlineDevicesStatistics();
-
-    deviceCount = data.devicesOnline;
-  } catch (e) {
-    console.error(e);
-
-    return {
-      ok: false,
-      error: 'Failed to fetch device count',
-    };
-  }
-
-  return { ok: true, deviceCount };
-}
