ded76cc562977bf5fe59d30ad41c3f25370be7d4
hhvrc <github@heavenvr.tech>
2025-07-17 14:53:24 +0200

    Move layout components to routes root

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
   import { page } from '$app/state';
   import { PUBLIC_DEVELOPMENT_BANNER } from '$env/static/public';
-  import AppSidebar from '$lib/components/layout/AppSidebar.svelte';
-  import Footer from '$lib/components/layout/Footer.svelte';
-  import Header from '$lib/components/layout/Header.svelte';
   import { BasicTags, OpenGraphTags, TwitterSummaryTags } from '$lib/components/metadata';
   import { SidebarProvider } from '$lib/components/ui/sidebar';
   import { Toaster } from '$lib/components/ui/sonner';
   import { buildMetaData } from '$lib/metadata';
   import { type Snippet, onMount } from 'svelte';
+  import Footer from './Footer.svelte';
+  import Header from './Header.svelte';
+  import Sidebar from './Sidebar.svelte';
   import '../app.css';
   import { initializeApp } from '$lib/init';
 
@@ -34,7 +34,7 @@
 <Toaster />
 
 <SidebarProvider bind:open={isOpen}>
-  <AppSidebar />
+  <Sidebar />
   <div class="flex h-screen w-screen flex-1 flex-col overflow-hidden">
     {#if PUBLIC_DEVELOPMENT_BANNER === 'true'}
       <div class="top-0 left-0 z-999 bg-[orangered] text-center text-white">
