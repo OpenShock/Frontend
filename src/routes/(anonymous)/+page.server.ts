abc530addcd4f3d3b8f7b11d0876f88ff854cacb
hhvrc <github@heavenvr.tech>
2025-07-17 16:28:06 +0200

    Maybe blocked because of headers?

--- src/routes/(anonymous)/+page.server.ts
+++ src/routes/(anonymous)/+page.server.ts
@@ -1,11 +1,37 @@
-import { metaApi } from '$lib/api';
+import { PUBLIC_BACKEND_API_DOMAIN } from '$env/static/public';
+import { Configuration, MetaApi } from '$lib/api/internal/v1';
 
-export async function load({ setHeaders }): Promise<{ ok: false, error: string } | { ok: true, deviceCount: number }> {
+type ResponseType = Promise<{ ok: false; error: string } | { ok: true; deviceCount: number }>;
+
+export async function load({ setHeaders }): ResponseType {
   setHeaders({
-    'cache-control': 'public, max-age=300'
-  })
+    'cache-control': 'public, max-age=300',
+  });
 
-  const { data } = await metaApi.publicGetOnlineDevicesStatistics();
+  let deviceCount: number;
 
-  return { ok: true, deviceCount: data.devicesOnline };
+  try {
+    const metaApi = new MetaApi(
+      new Configuration({
+        basePath: 'https://' + PUBLIC_BACKEND_API_DOMAIN,
+        headers: {
+          'User-Agent': 'OpenShockFrontend/1.0 (ServerSide)',
+        },
+        credentials: 'omit',
+      })
+    );
+
+    const { data } = await metaApi.publicGetOnlineDevicesStatistics();
+
+    deviceCount = data.devicesOnline;
+  } catch (e) {
+    console.error(e);
+
+    return {
+      ok: false,
+      error: 'Failed to fetch device count',
+    };
+  }
+
+  return { ok: true, deviceCount };
 }
