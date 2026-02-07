61f709ecb6f0555ef51541641d8adf6a7aa80f49
LucHeart <luc@luc.cat>
2026-02-07 22:35:31 +0100

    add logo

--- src/routes/(anonymous)/login/+page.svelte
+++ src/routes/(anonymous)/login/+page.svelte
@@ -1,8 +1,17 @@
 <script lang="ts">
   import Container from '$lib/components/Container.svelte';
   import LoginForm from '$lib/components/login-form.svelte';
+  import { asset, resolve } from '$app/paths';
 </script>
 
 <Container class="items-center justify-center">
+  <a href="##" class="flex items-center gap-2 self-center font-medium">
+    <img class="ml-[0.667px] h-7.5" src={asset('/IconSpinning.svg')} alt="OpenShock Logo" />
+    <img
+      class="h-7.5 transition-opacity delay-100 duration-200 group-data-[collapsible=icon]:opacity-0 group-data-[collapsible=icon]:delay-0"
+      src={asset('/LogoTextOnly.svg')}
+      alt="OpenShock Logo"
+    />
+  </a>
   <LoginForm />
 </Container>
